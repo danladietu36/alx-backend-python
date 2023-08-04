432e68683b824c1927e90a288d0fcdd5c6e52313
Danladi Etu <danladietugithub@example.com>
2023-08-04 22:32:05 +0000

    add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python module for type annotated function.
+"""
+
+
+def add(float: a, float: b) -> float:
+    """ Add function to sum up two floating
+        numbers.
+    """
+    return (a + b)
