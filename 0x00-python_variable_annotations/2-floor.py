268a54cfccbbc1e0ed45f44f72e7faadc01ef4ec
Danladi Etu <danladietugithub@example.com>
2023-08-04 22:48:42 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Python module for type annotated function.
+"""
+
+
+def floor(n: float) ->int:
+    """ Python function that takes float as
+       argument and returns integer.
+    """
+    return int(n)
