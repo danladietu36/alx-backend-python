9908a92ee7a45fc9b0fbdb627825d74d91403821
Danladi Etu <danladietugithub@example.com>
2023-08-07 23:03:27 +0000

    added task4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,15 +5,13 @@ task_wait_n. The code is nearly identical to wait_n except
 task_wait_random is being called.
 """
 
-
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Asyncronous coroutine that executes wait_random n times.
-    '''
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times"""
     tasks = []
     delays = []
 
