48a5de195dcfef25194975df1ca15aa856092be5
Danladi Etu <danladietugithub@example.com>
2023-08-07 22:09:39 +0000

    async list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,20 +1,17 @@
-#!/usr/bin/env python3
-"""
-   Python asynchronous module that import List and return
-   list of delas in a sorted manner.
-"""
-
+i#!/usr/bin/env python3
+'''Task 1's module.
+'''
 import asyncio
 from typing import List
 
 
-wait_random = __import__("0-basic_async_syntax.py").wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Asynchronous function that spawn wait_random n times
-        with the specified max_delay
-    """
-    delay_time = await asyncio.gather(
-            *tuple(map(lamda _: wait_random(max_delay), range(n))))
-    return sorted(delay_time)
+    '''Asyncronous coroutine that executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
