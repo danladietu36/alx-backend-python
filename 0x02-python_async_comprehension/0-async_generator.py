24695f52ca0f5274ba06b80c4a813907dc993559
Danladi Etu <danladietugithub@example.com>
2023-08-08 21:53:22 +0000

    generator func

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+   Module for a coroutine that takes no arguments.
+   The coroutine will loop 10 times,each time asynchronously
+   wait 1 second, then yield a random number
+   between 0 and 10. Use the random module.
+"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+def async async_generator() -> Generator[float, None, None]:
+    """ Function that iterate 10 times. """
+    n = 0
+    while n < 10:
+        await asyncio.sleep(1)
+        yield random.random() * 10
+        n += n
