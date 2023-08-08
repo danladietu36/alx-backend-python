ac0ea73819411403df331169cd42b4db23aa2d32
Danladi Etu <danladietugithub@example.com>
2023-08-08 22:19:30 +0000

    List comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,10 +11,10 @@
 
 
 from typing import List
-async_generator = __import__('0-async_generator.py').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """ loops 10 times. """
-    res = [i async for in async_generator()]
+    res = [i async for i in async_generator()]
     return res
