4e481893c285c2d9ae6494f6c49c16097450f1cf
Danladi Etu <danladietugithu@gmail.com>
2023-08-30 11:47:04 -0700

    unittest

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,65 +1,129 @@
 #!/usr/bin/env python3
-""" test utils.py """
+"""Familiarize yourself with the utils.access_nested_map function and
+understand its purpose. Play with it in the Python console to make
+sure you understand.
+
+In this task you will write the first unit test for utils.access_nested_map.
+
+Create a TestAccessNestedMap class that inherits from unittest.TestCase.
+
+Implement the TestAccessNestedMap.test_access_nested_map method to test that
+the method returns what it is supposed to.
+
+Decorate the method with @parameterized.expand to test the function for
+ollowing inputs:
+
+nested_map={"a": 1}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a",)
+nested_map={"a": {"b": 2}}, path=("a", "b")
+"""
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ unittests for nested map function """
+    """_summary_
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map, path, expected_value):
-        """ unittest for the function """
-        self.assertEqual(access_nested_map(nested_map, path), expected_value)
+    Args:
+        unittest (_type_): _description_
+    """
 
-    @parameterized.expand([
-        ({}, ('a',), KeyError('a')),
-        ({"a": 1}, ("a", "b"), KeyError('b'))
-    ])
-    def test_access_nested_map_exception(
-            self, nested_map, path, expected_value):
-        """ Unittest for exception in a nested map function """
-        with self.assertRaises(KeyError) as error:
-            access_nested_map(nested_map=nested_map, path=path)
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """_summary_
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
 
-        self.assertEqual(repr(error.exception), repr(expected_value))
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_output):
+        """_summary_
+        """
+        with self.assertRaises(expected_output) as context:
+            access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """ Test the response of json function """
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    def test_get_json(self, test_url: str, test_payload: dict):
-        """	unittest for correct execution """
-        with patch('requests.get') as mock_request:
-            mock_request.return_value.json.return_value = test_payload
-            self.assertEqual(get_json(url=test_url), test_payload)
+    """_summary_
+
+    Args:
+                    unittest (_type_): _description_
+    """
+    @parameterized.expand(
+        [
+            ('http://example.com', {'payload': True}),
+            ('http://holberton.io', {'payload': False})
+        ]
+    )
+    def test_get_json(self, url, expected_output):
+        """_summary_
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = expected_output
+        with patch('requests.get', return_value=mock_response):
+            response = get_json(url)
+
+            self.assertEqual(response, expected_output)
 
 
 class TestMemoize(unittest.TestCase):
-    """ Test the memoize decorator """
+    """_summary_
+
+    Args:
+                    unittest (_type_): _description_
+    """
 
     def test_memoize(self):
-        """	Test the memoize decorator that save the return of the function
-        that use it.
+        """_summary_
+
+        Returns:
+                _type_: _description_
         """
+
         class TestClass:
+            """_summary_
+            """
+
             def a_method(self):
+                """_summary_
+
+                Returns:
+                        _type_: _description_
+                """
                 return 42
 
             @memoize
             def a_property(self):
+                """_summary_
+
+                Returns:
+                        _type_: _description_
+                """
                 return self.a_method()
-        with patch.object(TestClass, 'a_method') as mock_memo:
-            test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            mock_memo.assert_called_once()
+
+        test_obj = TestClass()
+
+        with patch.object(test_obj, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
