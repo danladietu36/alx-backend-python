0534d3e139b4f8fb7a56064746d90d643008a731
Danladi Etu <danladietugithu@gmail.com>
2023-08-30 12:10:12 -0700

    added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,7 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-def test_access_nested_map(self, nested_map, path, expected_value):
+    def test_access_nested_map(self, nested_map, path, expected_value):
         """ unittest for the function """
         self.assertEqual(access_nested_map(nested_map, path), expected_value)
 
@@ -46,6 +46,7 @@ class TestGetJson(unittest.TestCase):
 
 class TestMemoize(unittest.TestCase):
     """ Test the memoize decorator """
+
     def test_memoize(self):
         """	Test the memoize decorator that save the return of the function
         that use it.
